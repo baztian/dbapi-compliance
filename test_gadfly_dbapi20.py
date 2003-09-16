b4b0b16acc9c0dee5010e8d081fa494b18388c4d
zenzen <>
2003-09-16 20:23:14 +0000

    Update gadfly dbapi20 test stub to work with newer version of the DB API 20 test suite

--- test_gadfly_dbapi20.py
+++ test_gadfly_dbapi20.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 '''
-$Id: test_gadfly_dbapi20.py,v 1.2 2003/02/12 09:48:46 zenzen Exp $
+$Id: test_gadfly_dbapi20.py,v 1.3 2003/09/17 03:23:14 zenzen Exp $
 '''
 
-__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.2 2003/02/12 09:48:46 zenzen Exp $'
-__version__ = '$Revision: 1.2 $'[11:-2]
+__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.3 2003/09/17 03:23:14 zenzen Exp $'
+__version__ = '$Revision: 1.3 $'[11:-2]
 __author__ = 'Stuart Bishop <zen@shangri-la.dropbear.id.au>'
 
 import unittest
@@ -12,11 +12,11 @@ import shutil
 import os
 import os.path
 import gadfly
-from dbapi20 import test_DBAPI20
+import dbapi20
 
 dbdir = os.path.join(os.path.dirname(__file__),'_db_dir')
 
-class test_GadflyDBAPI20(test_DBAPI20,unittest.TestCase):
+class test_GadflyDBAPI20(dbapi20.DatabaseAPI20Test):
     driver = gadfly
     connect_kw_args = {'dbname': 'test','dbdir': dbdir}
 
@@ -32,3 +32,6 @@ class test_GadflyDBAPI20(test_DBAPI20,unittest.TestCase):
     def test_callproc(self): pass
     def test_nextset(self): pass
     def test_setoutputsize(self): pass
+
+if __name__ == '__main__':
+    unittest.main()
