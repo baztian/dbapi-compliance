1624ad20f926db0bc579c558a2f130f2faf54b20
zenzen <>
2003-09-20 05:17:15 +0000

    connect args change for moellenbeck dbapi20 work

--- test_gadfly_dbapi20.py
+++ test_gadfly_dbapi20.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 '''
-$Id: test_gadfly_dbapi20.py,v 1.3 2003/09/17 03:23:14 zenzen Exp $
+$Id: test_gadfly_dbapi20.py,v 1.4 2003/09/20 12:17:15 zenzen Exp $
 '''
 
-__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.3 2003/09/17 03:23:14 zenzen Exp $'
-__version__ = '$Revision: 1.3 $'[11:-2]
+__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.4 2003/09/20 12:17:15 zenzen Exp $'
+__version__ = '$Revision: 1.4 $'[11:-2]
 __author__ = 'Stuart Bishop <zen@shangri-la.dropbear.id.au>'
 
 import unittest
@@ -18,7 +18,7 @@ dbdir = os.path.join(os.path.dirname(__file__),'_db_dir')
 
 class test_GadflyDBAPI20(dbapi20.DatabaseAPI20Test):
     driver = gadfly
-    connect_kw_args = {'dbname': 'test','dbdir': dbdir}
+    connect_kw_args = {'databasename': 'test', 'directory': dbdir}
 
     def setUp(self):
         if os.path.exists(dbdir):
