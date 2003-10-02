20d6d58b536a2c5e60d99711fd5949d5bbc81a94
moellenbeck <>
2003-10-02 09:41:01 +0000

    extend the test to create a test db and check the new dbapi 2.0 functionality provided by the gadfly modul

--- test_gadfly_dbapi20.py
+++ test_gadfly_dbapi20.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 '''
-$Id: test_gadfly_dbapi20.py,v 1.4 2003/09/20 12:17:15 zenzen Exp $
+$Id: test_gadfly_dbapi20.py,v 1.5 2003/10/02 16:41:01 moellenbeck Exp $
 '''
 
-__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.4 2003/09/20 12:17:15 zenzen Exp $'
-__version__ = '$Revision: 1.4 $'[11:-2]
+__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.5 2003/10/02 16:41:01 moellenbeck Exp $'
+__version__ = '$Revision: 1.5 $'[11:-2]
 __author__ = 'Stuart Bishop <zen@shangri-la.dropbear.id.au>'
 
 import unittest
@@ -14,7 +14,8 @@ import os.path
 import gadfly
 import dbapi20
 
-dbdir = os.path.join(os.path.dirname(__file__),'_db_dir')
+#dbdir = os.path.abspath(os.path.join(os.path.dirname(__file__),'_db_dir'))
+dbdir = os.path.abspath(os.path.join(".",'_db_dir'))
 
 class test_GadflyDBAPI20(dbapi20.DatabaseAPI20Test):
     driver = gadfly
@@ -24,11 +25,27 @@ class test_GadflyDBAPI20(dbapi20.DatabaseAPI20Test):
         if os.path.exists(dbdir):
             shutil.rmtree(dbdir)
         os.makedirs(dbdir)
+        g = gadfly.gadfly()
+        g.startup(self.connect_kw_args['databasename'],
+                  self.connect_kw_args['directory'])
+        c = g.cursor()
+        c.execute("select * from __table_names__")
+        c.execute("create table ph (nm varchar, ph varchar)")
+        g.commit()
 
     def tearDown(self):
         if os.path.exists(dbdir):
             shutil.rmtree(dbdir)
 
+    def test_None(self):
+        # gadfly parser does'nt understand NULL
+        pass
+
+    def test_description(self):
+        # only when the select-execute return an result, the types of the 
+        # description can determinat (see: gadfly/database.py line 390 ...)
+        pass
+            
     def test_callproc(self): pass
     def test_nextset(self): pass
     def test_setoutputsize(self): pass
