795d2a482561be000c16a13f49536da4263423ca
zenzen <>
2003-10-08 23:24:02 +0000

    dbapi20types.py fixes. Also, convert to using ISO8601 standard date/time.

--- test_gadfly_dbapi20.py
+++ test_gadfly_dbapi20.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 '''
-$Id: test_gadfly_dbapi20.py,v 1.5 2003/10/02 16:41:01 moellenbeck Exp $
+$Id: test_gadfly_dbapi20.py,v 1.6 2003/10/09 06:24:02 zenzen Exp $
 '''
 
-__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.5 2003/10/02 16:41:01 moellenbeck Exp $'
-__version__ = '$Revision: 1.5 $'[11:-2]
+__rcs_id__  = '$Id: test_gadfly_dbapi20.py,v 1.6 2003/10/09 06:24:02 zenzen Exp $'
+__version__ = '$Revision: 1.6 $'[11:-2]
 __author__ = 'Stuart Bishop <zen@shangri-la.dropbear.id.au>'
 
 import unittest
@@ -49,6 +49,33 @@ class test_GadflyDBAPI20(dbapi20.DatabaseAPI20Test):
     def test_callproc(self): pass
     def test_nextset(self): pass
     def test_setoutputsize(self): pass
+
+    def test_ISO8601(self):
+        self.failUnlessEqual(str(gadfly.Date(1234,12,21)), '1234-12-21')
+        self.failUnlessEqual(str(gadfly.Date(34,2,4)), '0034-02-04')
+        self.failUnlessEqual(str(gadfly.Time(00,01,02)), '00:01:02Z')
+        self.failUnlessEqual(str(gadfly.Time(24,01,02)), '24:01:02Z')
+        self.failUnlessEqual(
+                str(gadfly.Timestamp(1234,12,21,00,01,02)),
+                '1234-12-21T00:01:02Z'
+                )
+        self.failUnlessEqual(
+            str(gadfly.Timestamp(34,2,4,24,01,02)),
+            '0034-02-04T24:01:02Z'
+            )
+
+        # Add 8 hours, 21 mins and 2 secs as midnight isn't a good test
+        groundhog_day = 1044144000 + 8*60*60 + 21*60 + 2
+        self.failUnlessEqual(
+                str(gadfly.DateFromTicks(groundhog_day)), '2003-02-02'
+                )
+        self.failUnlessEqual(
+                str(gadfly.TimeFromTicks(groundhog_day)), '08:21:02Z'
+                )
+        self.failUnlessEqual(
+                str(gadfly.TimestampFromTicks(groundhog_day)),
+                '2003-02-02T08:21:02Z'
+                )
 
 if __name__ == '__main__':
     unittest.main()
