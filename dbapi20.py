9645b5f10d02aff0482db77bc7b5c00cd96fc24a
baztian <bastian.dev@gmail.com>
2014-02-15 19:42:01 +0100

    Use Bumpversion

--- dbapi20.py
+++ dbapi20.py
@@ -11,8 +11,7 @@
     -- Ian Bicking
 '''
 
-__version__ = '$Revision: 1.14.3 $'[11:-2]
-__author__ = 'Stuart Bishop <stuart@stuartbishop.net>'
+__version__ = '1.14.3'
 
 import unittest
 import time
@@ -27,63 +26,6 @@ else:                   #python 2.x
     def _failUnless(self, expr, msg=None):
         self.failUnless(expr, msg)  ## deprecated since Python 2.6
 
-# Revision 1.14  2013/05/20 11:02:05  kf7xm
-# Add a literal string to the format insertion test to catch trivial re-format algorthims
-
-# Revision 1.13  2013/05/08 14:31:50  kf7xm
-# Quick switch to Turn off IDEMPOTENT_CLOSE test. Also: Silence teardown failure
-
-# Revision 1.12  2009/02/06 03:35:11  kf7xm
-# Tested okay with Python 3.0, includes last minute patches from Mark H.
-#
-# Revision 1.1.1.1.2.1  2008/09/20 19:54:59  rupole
-# Include latest changes from main branch
-# Updates for py3k
-#
-# Revision 1.11  2005/01/02 02:41:01  zenzen
-# Update author email address
-#
-# Revision 1.10  2003/10/09 03:14:14  zenzen
-# Add test for DB API 2.0 optional extension, where database exceptions
-# are exposed as attributes on the Connection object.
-#
-# Revision 1.9  2003/08/13 01:16:36  zenzen
-# Minor tweak from Stefan Fleiter
-#
-# Revision 1.8  2003/04/10 00:13:25  zenzen
-# Changes, as per suggestions by M.-A. Lemburg
-# - Add a table prefix, to ensure namespace collisions can always be avoided
-#
-# Revision 1.7  2003/02/26 23:33:37  zenzen
-# Break out DDL into helper functions, as per request by David Rushby
-#
-# Revision 1.6  2003/02/21 03:04:33  zenzen
-# Stuff from Henrik Ekelund:
-#     added test_None
-#     added test_nextset & hooks
-#
-# Revision 1.5  2003/02/17 22:08:43  zenzen
-# Implement suggestions and code from Henrik Eklund - test that cursor.arraysize
-# defaults to 1 & generic cursor.callproc test added
-#
-# Revision 1.4  2003/02/15 00:16:33  zenzen
-# Changes, as per suggestions and bug reports by M.-A. Lemburg,
-# Matthew T. Kromer, Federico Di Gregorio and Daniel Dittmar
-# - Class renamed
-# - Now a subclass of TestCase, to avoid requiring the driver stub
-#   to use multiple inheritance
-# - Reversed the polarity of buggy test in test_description
-# - Test exception heirarchy correctly
-# - self.populate is now self._populate(), so if a driver stub
-#   overrides self.ddl1 this change propogates
-# - VARCHAR columns now have a width, which will hopefully make the
-#   DDL even more portible (this will be reversed if it causes more problems)
-# - cursor.rowcount being checked after various execute and fetchXXX methods
-# - Check for fetchall and fetchmany returning empty lists after results
-#   are exhausted (already checking for empty lists if select retrieved
-#   nothing
-# - Fix bugs in test_setoutputsize_basic and test_setinputsizes
-#
 def str2bytes(sval):
     if sys.version_info < (3,0) and isinstance(sval, str):
         sval = sval.decode("latin1")
